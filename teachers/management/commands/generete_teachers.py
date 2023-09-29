75b00db1b65e62608d2e8437d2d74197a8004389
Nazar Bezmenov <nazarbezmenov1@gmail.com>
2023-09-29 22:08:54 +0100

    Added command

---
+++ teachers/management/commands/generete_teachers.py
@@ -0,0 +1,23 @@
+import faker
+
+from django.core.management.base import BaseCommand
+from teachers.models import Teacher
+
+fake = faker.Faker()
+
+class Command(BaseCommand):
+    help = "Add the specified number of teachers to the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--number", type=int, default=100)
+
+    def handle(self, *args, **options):
+        for i in range (options["number"]):
+            t = Teacher.objects.create(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                birth_date=fake.date()
+            )
+            self.stdout.write(
+                self.style.SUCCESS('Successfully added teacher "%s"' % t.id)
+            )
