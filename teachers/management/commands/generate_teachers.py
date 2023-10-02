7d7edbb77f51a6e16d304f0baf7f2709a5ea8179
Nazar Bezmenov <nazarbezmenov1@gmail.com>
2023-10-02 15:29:08 +0100

    Added missing files

--- teachers/management/commands/generate_teachers.py
+++ teachers/management/commands/generate_teachers.py
@@ -5,6 +5,7 @@ from teachers.models import Teacher
 
 fake = faker.Faker()
 
+
 class Command(BaseCommand):
     help = "Add the specified number of teachers to the database"
 
@@ -14,14 +15,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         number = options["number"]
         if number < 1:
-            self.stdout.write(
-                self.style.ERROR("Number is less then 1")
-            )
-        for i in range (options["number"]):
+            self.stdout.write(self.style.ERROR("Number is less then 1"))
+        for i in range(options["number"]):
             t = Teacher.objects.create(
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
-                birth_date=fake.date()
+                birth_date=fake.date(),
             )
             self.stdout.write(
                 self.style.SUCCESS('Successfully added teacher "%s"' % t.id)
--- teachers/views.py
+++ teachers/views.py
@@ -3,9 +3,12 @@ from teachers.models import Teacher
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the students index.")
+
+
 def teachers_view(request):
     teachers = Teacher.objects.order_by("id")
-    text = " ".join([str(q)+"<br>" for q in teachers])
+    text = " ".join([str(q) + "<br>" for q in teachers])
     return HttpResponse(text)
